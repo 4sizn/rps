a3a013e65c8fef6e2a30743c568304d1342dd016
hsshin <hsshin@rsupport.com>
2022-07-17 15:27:47 +0900

    feat: make random uuid using Crypto api

--- src/rps.ts
+++ src/rps.ts
@@ -13,7 +13,7 @@ export function game(players: Player[]): GAME_RESULT {
 export function makePlayer(hand: HAND): Player {
   assert(hand >= 0 && hand < 3, "player hands problem...");
   return {
-    id: getUUID(),
+    id: new Crypto().randomUUID(),
     hand: hand,
   };
 }
@@ -22,15 +22,6 @@ export function rpsResult(myHand: HAND, otherHand: HAND): RPS_RESULT {
   return [RPS_RESULT.WIN, RPS_RESULT.LOSE, RPS_RESULT.DRAW][
     (myHand - otherHand + 2) % 3
   ];
-}
-
-// UUID v4 generator in JavaScript (RFC4122 compliant)
-function getUUID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 3) | 8;
-    return v.toString(16);
-  });
 }
 
 function assert(condition: boolean, message: string) {
